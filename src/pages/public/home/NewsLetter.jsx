6bc8bdff6f8e009c5edb71099845f2051f5fdd94
AbdulHameed <youngyusuff006@gmail.com>
2022-02-18 14:18:09 +0100

    New homepage

--- src/pages/public/home/NewsLetter.jsx
+++ src/pages/public/home/NewsLetter.jsx
@@ -4,10 +4,9 @@ const Newsletter = ()=>{
   return (<div class="fullBody">
             <form class="Body text-center text-white">
                 <p class="text-center text-white fw-bold pt-4 new">Subscribe to our Newsletter</p>
-                <label class="label me-1">Email: </label>
-                <p class="text-center btn-group">    
-                <input type="email" placeholder='Enter your email' class="rounded px-5 w-100"/>
-                    <input type="submit" value="Subscribe" class="btn btn-outline-dark rounded"/>
+                <p class="text-center btn-group rounded-pill">    
+                <input type="email" class="form-control btn" placeholder="Enter your e-mail address"/>
+                <button class="btn btn-primary" type="button"> <b>Submit</b></button>
                 </p>
             </form>
 
@@ -16,3 +15,4 @@ const Newsletter = ()=>{
 }
 export default Newsletter
 ;
+
