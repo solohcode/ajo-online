82b99338ff5ee4a1abca15456af9911c100fad34
kabiruola <kabiruola1234@gmail.com>
2022-02-21 10:36:47 -0800

    my page

--- src/router.js
+++ src/router.js
@@ -28,6 +28,11 @@ const routes = [
     exact: true,
   },
   {
+    path: '/auth/chnage-password',
+    Component: lazy(() => import('pages/auth/change-password')),
+    exact: true,
+  },
+  {
     path: '/auth/register',
     Component: lazy(() => import('pages/auth/register')),
     exact: true,
